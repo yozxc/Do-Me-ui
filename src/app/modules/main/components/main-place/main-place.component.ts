54e80b20e2ca1b0e9365f3c3d6f63dda527d4795
jhonny919 <ionutmitu67@gmail.com>
2022-01-18 01:32:13 +0200

    add: minor mainPlace changes + todo fix by observables

--- src/app/modules/main/components/main-place/main-place.component.ts
+++ src/app/modules/main/components/main-place/main-place.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-place',
-  templateUrl: './main-place.component.html',
-  styleUrls: ['./main-place.component.scss']
+    selector: 'app-main-place',
+    templateUrl: './main-place.component.html',
+    styleUrls: ['./main-place.component.scss'],
 })
 export class MainPlaceComponent implements OnInit {
+    _isActive: boolean = true;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    toggleIsActive() {
+        this._isActive = !this._isActive;
+    }
 }
