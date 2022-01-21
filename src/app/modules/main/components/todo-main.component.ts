534548617de9761aacf6eecb9667d48b0bc9e667
Ion Mitu <c-ion.mitu@edifecs.com>
2022-01-21 15:50:01 +0200

    add: minor stuff's

--- src/app/modules/main/components/todo-main.component.ts
+++ src/app/modules/main/components/todo-main.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-main',
-  templateUrl: './todo-main.component.html',
-  styleUrls: ['./todo-main.component.scss']
+    selector: 'app-todo-main',
+    templateUrl: './todo-main.component.html',
+    styleUrls: ['./todo-main.component.scss'],
 })
 export class TodoMainComponent implements OnInit {
+    menuIsClosed: boolean = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    toggleMenuIsClosed() {
+        this.menuIsClosed = !this.menuIsClosed;
+        console.log(this.menuIsClosed);
+    }
 }
