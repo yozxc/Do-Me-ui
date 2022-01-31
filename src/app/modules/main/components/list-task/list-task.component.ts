db680d9cc66f528d2fe4d8560fe025605bb1b721
jhonny919 <ionutmitu67@gmail.com>
2022-01-31 23:03:21 +0200

    fix: list task minor fix

--- src/app/modules/main/components/list-task/list-task.component.ts
+++ src/app/modules/main/components/list-task/list-task.component.ts
@@ -1,16 +1,21 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { v4 as uuidv4, v4 } from 'uuid';
 
 @Component({
-  selector: 'app-list-task',
-  templateUrl: './list-task.component.html',
-  styleUrls: ['./list-task.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-list-task',
+    templateUrl: './list-task.component.html',
+    styleUrls: ['./list-task.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListTaskComponent implements OnInit {
+    isChecked!: boolean;
+    checkboxID = v4();
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    toggleIsChecked() {
+        this.isChecked = !this.isChecked;
+    }
 }
