be637bb71b36b112f24be731d5af2d342982f698
jhonny919 <ionutmitu67@gmail.com>
2022-02-03 18:04:33 +0200

    add: auth module views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoMainComponent } from './modules/main/components/todo-main.component';
 import { SignPageComponent } from './modules/auth/components/sign-page/sign-page.component';
 import { RegisterPageComponent } from './modules/auth/components/register-page/register-page.component';
+import { ForgotPasswordPageComponent } from './modules/auth/components/forgotPass-page/forgot-password-page.component';
+import { PasswordResetPageComponent } from './modules/auth/components/password-reset-page/password-reset-page.component';
 
 enum ROUTE {
     AUTH = 'auth',
-    SIGNIN = 'signin',
+    SIGNIN = 'signIn',
     REGISTER = 'register',
+    FORGOTPASSWORD = 'forgotPassword',
+    RESETPASSWORD = 'resetPassword',
 
     MAIN = 'main',
 }
@@ -19,6 +23,8 @@ const routes: Routes = [
     { path: ``, redirectTo: `${ROUTE.AUTH}/${ROUTE.SIGNIN}`, pathMatch: 'full' },
     { path: `${ROUTE.AUTH}/${ROUTE.SIGNIN}`, component: SignPageComponent },
     { path: `${ROUTE.AUTH}/${ROUTE.REGISTER}`, component: RegisterPageComponent },
+    { path: `${ROUTE.AUTH}/${ROUTE.FORGOTPASSWORD}`, component: ForgotPasswordPageComponent },
+    { path: `${ROUTE.AUTH}/${ROUTE.RESETPASSWORD}`, component: PasswordResetPageComponent },
 
     // todo : dev temp
     { path: `**`, redirectTo: `${ROUTE.MAIN}`, pathMatch: 'full' },
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignPageComponent } from './components/sign-page/sign-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ForgotPasswordPageComponent } from './components/forgotPass-page/forgot-password-page.component';
+import { PasswordResetPageComponent } from './components/password-reset-page/password-reset-page.component';
 
 import { FacebookIconComponent } from './components/svg/facebook-icon.component';
 import { GoogleIconComponent } from './components/svg/google-icon.component';
@@ -14,13 +16,15 @@ import { OpenEyeComponent } from './components/svg/open-eye.component';
     declarations: [
         SignPageComponent,
         RegisterPageComponent,
+        ForgotPasswordPageComponent,
+        PasswordResetPageComponent,
         //
         FacebookIconComponent,
         GoogleIconComponent,
         CloseEyeComponent,
         OpenEyeComponent,
     ],
-    exports: [SignPageComponent, RegisterPageComponent],
+    exports: [SignPageComponent, RegisterPageComponent, ForgotPasswordPageComponent, PasswordResetPageComponent],
     imports: [CommonModule, RouterModule],
 })
 export class AuthModule {}
