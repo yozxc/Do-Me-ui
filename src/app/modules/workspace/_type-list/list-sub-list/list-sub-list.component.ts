a228ca23a4296007bee976f48c51bae4fea60b84
Ion Mitu <c-ion.mitu@edifecs.com>
2022-06-20 11:14:19 +0300

    fix: sub-list title save

--- src/app/modules/workspace/_type-list/list-sub-list/list-sub-list.component.ts
+++ src/app/modules/workspace/_type-list/list-sub-list/list-sub-list.component.ts
@@ -1,8 +1,9 @@
 import { BehaviorSubject } from 'rxjs';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { TaskData } from '@app/core/types/taskData';
 import { AddTaskData } from '@app/core/types/addTaskData';
 import { v4 } from 'uuid';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-list-sub-list',
@@ -10,14 +11,24 @@ import { v4 } from 'uuid';
     styleUrls: ['./list-sub-list.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListSubListComponent {
+export class ListSubListComponent implements OnInit {
     isChangingTitle: boolean = false;
+    changeButtonDisabled: boolean = false;
     isClosed$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-    @Input() listTitle: string = 'Overdue';
+    @Input() editableTitle: boolean = true;
+
+    @Input() listTitle!: string;
     @Input() tasksList?: TaskData[];
 
+    titleFormControl!: FormControl;
+
     constructor() {}
+
+    ngOnInit(): void {
+        this.titleFormControl = new FormControl(this.listTitle);
+        this.titleFormControl.valueChanges.subscribe((value) => (this.changeButtonDisabled = !value.length));
+    }
 
     saveTitle(listTitle: string) {
         this.listTitle = listTitle;
@@ -35,4 +46,14 @@ export class ListSubListComponent {
             isChecked: false
         });
     }
+
+    onSave() {
+        if (this.titleFormControl.value.length) {
+            // todo : save to back
+            this.listTitle = this.titleFormControl.value;
+            this.isChangingTitle = false;
+        }
+    }
+
+    onCancel() {}
 }
