dc8e306808a33af0613228fe1cc612e95ff1f427
jhonny919 <ionutmitu67@gmail.com>
2022-07-05 12:53:41 +0300

    add: akita packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
-import { ProjectsService } from '@core/services/data/projects/projects.service';
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { WorkspaceModule } from './modules/workspace/workspace.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ModalsModule } from './modules/modals/modals.module';
 
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { ProjectsService } from '@core/services/data/projects/projects.service';
+
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, WorkspaceModule, AuthModule, ModalsModule],
-    providers: [ProjectsService],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        WorkspaceModule,
+        AuthModule,
+        ModalsModule,
+        environment.production ? [] : AkitaNgDevtools.forRoot(),
+        AkitaNgRouterStoreModule
+    ],
+    providers: [ProjectsService, { provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' } }],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
